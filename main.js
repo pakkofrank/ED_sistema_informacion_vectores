cbb472bb31f85f1649e88e132381141d0cd7e967
pakkofrank <jpalma@ucol.mx>
2019-09-20 12:05:24 -0500

    ya agrega esta cosa

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ import Tienda from "./Tienda.js";
 
 class Main{
     constructor(){
-        let 
+        let tienda = new Tienda(document.querySelector("#tabla"));
+
+        document.querySelector("#agregar").addEventListener('click', ()=>{
+            let codigo = Number(document.querySelector("#codigo").value);
+            let nombre = document.querySelector("#nombre").value;
+            let precio = Number(document.querySelector("#precio").value);
+            let cantidad = Number(document.querySelector("#cantidad").value);
+            let descripcion = document.querySelector("#descripcion").value;
+
+            let objProducto = {
+                codigo:codigo,
+                nombre:nombre,
+                precio:precio,
+                cantidad:cantidad,
+                descripcion:descripcion
+
+            }
+
+            let producto = new Productos(objProducto);
+
+            tienda.agregarProducto(producto);
+        });
     }
 }
+
+let m = new Main();
