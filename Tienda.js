466b2bb254a1677404c2300373783f9c2f08da2f
pakkofrank <jpalma@gmail.com>
2019-09-20 17:19:29 -0500

    ya elimina

--- Tienda.js
+++ Tienda.js
@@ -11,12 +11,14 @@ export default class Tienda{
         let cellPrecio = row.insertCell(2);
         let cellCantidad = row.insertCell(3);
         let cellDescripcion = row.insertCell(4);
+        row.insertCell(5);
 
         cellCodigo.innerHTML = producto.codigo;
         cellNombre.innerHTML = producto.nombre;
         cellPrecio.innerHTML = producto.precio;
         cellCantidad.innerHTML = producto.cantidad;
         cellDescripcion.innerHTML = producto.descripcion;
+        this._addBtnEliminar(row, producto);
 
         let objProductos = {
             codigo:producto.codigo,
@@ -29,7 +31,7 @@ export default class Tienda{
         this._vector.push(objProductos);
     }
 
-    buscarProducto(codigo){
+    _buscarProducto(codigo){
         let result = -1;
         this._vector.forEach((producto, index) => {
             if(producto.codigo === codigo){
@@ -50,4 +52,38 @@ export default class Tienda{
         })
     }
 
-}
+    _addBtnEliminar(row, producto){
+        let btnEliminar = document.createElement("input");
+        btnEliminar.value = "Eliminar";
+        btnEliminar.type = "button";
+        btnEliminar.className = "btn btn-danger";
+        btnEliminar.id = "btnEliminar";
+
+        btnEliminar.addEventListener("click", ()=>{
+            let p = new Tienda();
+            p._eliminarProducto(row, producto)
+        });
+        row.cells[5].innerHTML = "";
+        row.cells[5].appendChild(btnEliminar);
+
+    }
+
+    _eliminarProducto(row, articulo){
+        Swal.fire({
+            type: "question",
+            title: "¿Deseas eliminar al contacto?",
+            text: articulo.codigo,
+            showCancelButton: true,
+            confirmButtonText: "Sí",
+            cancelButtonText: "No"
+        }).then(result => {
+            if (result.value) {
+                let r = this._buscarProducto(articulo.codigo);
+                this._vector.splice(r, 1);
+                row.remove();
+            }
+        });
+      }
+
+
+    }
